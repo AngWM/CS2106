0ace7806302de05431ca3ecfaac201fae7282d6a
WM <reid_winterpurity@live.com>
2018-03-15 15:06:00 +0800

    Add assg2p4.c

---
+++ cs2106lab4/assg2p4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int glob;
+
+void *child(void *t)
+{
+	// Increment glob by 1, wait for 1 second, then increment by 1 again.
+	printf("Child %d entering. Glob is currently %d\n", t, glob);
+	glob++
+	sleep(1);
+	glob++;
+	printf("Child %d exiting. Glob is currently %d\n", t, glob);	
+}
+
+int main()
+{
+	int i;
+	glob = 0;
+
+	for(i=0; i<10; i++)
+		child((void *) i);
+
+	printf("Final value of glob is %d\n", glob);
+	return 0;
+}
